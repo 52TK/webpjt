812b51d93fea0b8574590607ed99e857f9927c27
52TK <0910ydn@gamil.com>
2022-01-12 15:54:57 +0900

    DB세팅 및 Security Config 설정

---
+++ src/main/java/com/ydn/EaglesCM/EaglesCmApplication.java
@@ -0,0 +1,13 @@
+package com.ydn.EaglesCM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaglesCmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EaglesCmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ydn/EaglesCM/EaglesCmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ydn.EaglesCM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaglesCmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
