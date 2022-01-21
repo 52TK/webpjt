efc8201b91fa2f9a09302d84f3a9f811aa225e0c
52TK <0910ydn@gmail.com>
2022-01-21 14:13:16 +0900

    Board List 구현

--- src/main/java/com/ydn/EaglesCM/controller/BoardController.java
+++ src/main/java/com/ydn/EaglesCM/controller/BoardController.java
@@ -1,5 +1,6 @@
 package com.ydn.EaglesCM.controller;
 
+import com.ydn.EaglesCM.domain.Board;
 import com.ydn.EaglesCM.dto.board.BoardSaveForm;
 import com.ydn.EaglesCM.service.BoardService;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +8,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -28,6 +31,18 @@ public class BoardController {
         boardService.save(boardSaveForm);
 
         return "redirect:/";
+    }
+
+    // list 작업
+    @GetMapping("/boards")
+    public String showBoardList(Model model){
+
+        List<Board> boardList = boardService.findAll();
+
+        model.addAttribute("boardList", boardList);
+
+        return "usr/board/list";
+
     }
 
 }
--- src/main/java/com/ydn/EaglesCM/service/BoardService.java
+++ src/main/java/com/ydn/EaglesCM/service/BoardService.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -26,5 +28,8 @@ public class BoardService{
 
     }
 
+    public List<Board> findAll() {
+        return boardRepository.findAll();
+    }
 }
 
