912e3fa684bbba98aa5f42f002a5b67d57f5313d
52TK <0910ydn@gmail.com>
2022-01-27 14:17:50 +0900

    게시판 수정 개선

--- src/main/java/com/ydn/EaglesCM/dto/board/BoardModifyForm.java
+++ src/main/java/com/ydn/EaglesCM/dto/board/BoardModifyForm.java
@@ -1,8 +1,10 @@
 package com.ydn.EaglesCM.dto.board;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class BoardModifyForm {
 
     private String name;
