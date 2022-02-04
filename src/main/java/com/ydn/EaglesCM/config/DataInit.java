5964ed1330aad567baf329e54e138a60d6600a51
52TK <0910ydn@gmail.com>
2022-02-04 14:53:15 +0900

    동작 수정

--- src/main/java/com/ydn/EaglesCM/config/DataInit.java
+++ src/main/java/com/ydn/EaglesCM/config/DataInit.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -27,7 +28,7 @@ public class DataInit {
     }
 
     @Component
-    @Service
+    @Transactional
     @RequiredArgsConstructor
     static class InitService {
 
