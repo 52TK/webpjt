7244e93bf7a912435a0192ef008483cad43f4b48
52TK <0910ydn@gmail.com>
2022-02-07 15:48:27 +0900

    @NotBlack 회원가입 수정

--- src/main/java/com/ydn/EaglesCM/dto/member/MemberSaveForm.java
+++ src/main/java/com/ydn/EaglesCM/dto/member/MemberSaveForm.java
@@ -6,16 +6,16 @@ import javax.validation.constraints.NotBlank;
 
 @Data
 public class MemberSaveForm {
-    // 회원가입시 필요한 정보
-    @NotBlank
+    // 회원가입시 필요한 정보, null 불가
+    @NotBlank(message = "아이디를 입력해 주세요.")  // 기본 셋팅시 "공백일 수 없습니다." 멘트로 설정됨
     private String loginId;
-    @NotBlank
+    @NotBlank(message = "비밀번호를 입력해 주세요.")
     private String loginPw;
-    @NotBlank
+    @NotBlank(message = "이름을 입력해 주세요.")
     private String name;
-    @NotBlank
+    @NotBlank(message = "닉네임을 입력해 주세요.")
     private String nickname;
-    @NotBlank
+    @NotBlank(message = "이메일을 입력해 주세요.")
     private String email;
 
 }
