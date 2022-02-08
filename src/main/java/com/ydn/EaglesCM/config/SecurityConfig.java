d25b1605a4a929ad93bb566f20347ccebc53ba82
52TK <0910ydn@gmail.com>
2022-02-08 15:10:29 +0900

    회원정보 에러표현 로직개선

--- src/main/java/com/ydn/EaglesCM/config/SecurityConfig.java
+++ src/main/java/com/ydn/EaglesCM/config/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         .mvcMatchers(
                                 "/articles/**",
                                 "/",
-                                "/members/modify"
+                                "/members/modify/**"
 
                         )
                         .permitAll()
