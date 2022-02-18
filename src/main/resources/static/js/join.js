92752de86c40e7a6066d8cde96b2bea52187d250
52TK <0910ydn@gmail.com>
2022-02-18 15:48:32 +0900

    회원가입시 닉네임 중복확인

--- src/main/resources/static/js/join.js
+++ src/main/resources/static/js/join.js
@@ -1,6 +1,8 @@
 let CHECK_STATUS = false;
 let LOGIN_ID_STATUS = false;
+let NICKNAME_STATUS = false;
 
+// 아이디 중복확인
 async function checkDupleLoginId(){
 
     let inputLoginId = document.querySelector("#loginId");
@@ -40,9 +42,50 @@ async function checkDupleLoginId(){
     )
 }
 
+// 닉네임 중복확인
+async function checkDupleNickname(){
+
+    let inputNickname = document.querySelector("#nickname");
+    let nickname = inputNickname.value;
+
+    await fetch("http://localhost:8085/members/check/nickname?nickname=" + nickname)
+    .then(
+        (response) => {
+            return response.json();
+        }
+    )
+    .then(
+        (data) => {
+
+            let nicknameCheck = data;
+
+            console.log("dataCheck")
+            console.log(dataCheck);
+            console.log("dataCheck.status")
+            console.log(dataCheck.status);
+
+            if(nicknameCheck.status || nickname === ""){
+                NICKNAME_STATUS = false;
+                console.log(NICKNAME_STATUS);
+                alert("가입하실 수 없는 닉네임 입니다.")
+            }else{
+                NICKNAME_STATUS = true;
+                console.log(NICKNAME_STATUS);
+                alert("가입하실 수 있는 닉네임 입니다.")
+            }
+        }
+    )
+    .catch(
+        (error) => {
+            console.log(error);
+        }
+    )
+}
+
+// 안건드림
 function checkStatus(){
 
-    if(LOGIN_ID_STATUS){
+    if(LOGIN_ID_STATUS && NICKNAME_STATUS){
         CHECK_STATUS = true;
     }else{
         CHECK_STATUS = false;
