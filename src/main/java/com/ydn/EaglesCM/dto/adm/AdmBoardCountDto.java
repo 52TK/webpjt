979f743a5f03d551402d8700e7fee9c434da16c0
52TK <0910ydn@gmail.com>
2022-03-10 15:46:59 +0900

    게시판관리 기능 1차

---
+++ src/main/java/com/ydn/EaglesCM/dto/adm/AdmBoardCountDto.java
@@ -0,0 +1,11 @@
+package com.ydn.EaglesCM.dto.adm;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdmBoardCountDto {
+}
---
+++ src/main/java/com/ydn/EaglesCM/dto/adm/BoardStatDto.java
@@ -0,0 +1,14 @@
+package com.ydn.EaglesCM.dto.adm;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BoardStatDto {
+
+    private Long totalBoardCount;
+
+}
